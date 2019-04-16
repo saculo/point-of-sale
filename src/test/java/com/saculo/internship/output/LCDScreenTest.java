33349d8882c84f1ebb20a51dd15a2a707c607ec0
saculo <lukasz.grula@gmail.com>
2019-04-16 14:29:19 +0200

    Added test package

---
+++ src/test/java/com/saculo/internship/output/LCDScreenTest.java
@@ -0,0 +1,14 @@
+package com.saculo.internship.output;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class LCDScreenTest {
+
+    @BeforeEach
+    public void init() {}
+
+    @Test
+    public void shouldPrintIfProductDoesntExist() {}
+
+}
